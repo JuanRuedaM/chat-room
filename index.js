c2b983b5c36b99c15ad352fce629c49ea6df5a99
JuanRuedaM <juanruedam1@gmail.com>
2023-08-15 12:20:06 -0500

    New Host in react to connect

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ app.use((req, res, next) => {
   res.status(404).send("<h1>NOT FOUND</h1>");
 });
 
-server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`Server running on ${__dirname}:${PORT}`))
